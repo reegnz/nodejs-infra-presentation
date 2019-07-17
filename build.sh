5dcf4595d8e53a5f6ae40a38b5683667fcb3cfdf
Zoltán Reegn <zoltan.reegn@gmail.com>
2019-07-18 01:08:29 +0200

    Update build script `

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #! /bin/sh -e
-pandoc --standalone --to revealjs --slide-level=2 -o dist/index.html src/config.yaml src/chapters/00_introduction.md src/chapters/01_npm.md src/chapters/02_os.md src/chapters/04_health.md src/chapters/04_service_lifecycle.md src/chapters/05_logs.md src/chapters/06_http_proxies.md src/chapters/99_sre.md
+pandoc --standalone --to revealjs --slide-level=2 -o dist/index.html src/config.yaml src/chapters/00_introduction.md src/chapters/01_npm.md src/chapters/02_os.md src/chapters/04_health.md src/chapters/05_logs.md src/chapters/06_http_proxies.md src/chapters/50_end.md src/chapters/99_service_lifecycle.md src/chapters/99_sre.md
